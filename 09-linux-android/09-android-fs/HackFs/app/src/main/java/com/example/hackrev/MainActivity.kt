377c8f96af84aa81b7869f56727514d956aa9b02
cocomelonc <cocomelonkz@gmail.com>
2025-04-26 11:42:53 +0300

    android fs update permission logic

--- 09-linux-android/09-android-fs/HackFs/app/src/main/java/com/example/hackrev/MainActivity.kt
+++ 09-linux-android/09-android-fs/HackFs/app/src/main/java/com/example/hackrev/MainActivity.kt
@@ -1,48 +1,87 @@
 package com.example.hackrev
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hackrev.ui.theme.HackRevTheme
+import android.provider.DocumentsContract
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import java.io.InputStream
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            HackRevTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+class MainActivity : AppCompatActivity() {
+
+    // request permission
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // if permission is ok open SAF
+                openFile()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    // request permissions
+    private val requestManageExternalStorageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val isGranted =
+                    Environment.isExternalStorageManager() // Проверяем разрешение MANAGE_EXTERNAL_STORAGE
+                if (isGranted) {
+                    openFile()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-        System.loadLibrary("reverse-shell")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // check permissions for READ_EXTERNAL_STORAGE
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        ) {
+            openFile()
+        } else {
+            // Запрашиваем разрешение
+            requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Meow-meow!!!! Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun openFile() {
+        // open file via Storage Access Framework (SAF)
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.type = "*/*" // Указываем тип файлов, которые мы хотим открыть
+        startActivityForResult(intent, 42)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HackRevTheme {
-        Greeting("Bahrain")
+    // working with file
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 42 && resultCode == RESULT_OK) {
+            val uri: Uri? = data?.data
+            uri?.let {
+                readFile(it)
+            }
+        }
+    }
+
+    private fun readFile(uri: Uri) {
+        try {
+            val inputStream: InputStream? = contentResolver.openInputStream(uri)
+            inputStream?.let { stream ->
+                val fileContent = stream.bufferedReader().use { it.readText() }
+                Toast.makeText(this, fileContent, Toast.LENGTH_LONG).show()
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error reading file: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
