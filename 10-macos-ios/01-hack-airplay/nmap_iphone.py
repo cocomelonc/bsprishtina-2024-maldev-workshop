df040263e54e032cbea7e4de83c1b9b0c4551144
cocomelonc <cocomelonkz@gmail.com>
2025-05-08 14:35:35 +0300

    nmap iphone add nmap hosts scan

--- 10-macos-ios/01-hack-airplay/nmap_iphone.py
+++ 10-macos-ios/01-hack-airplay/nmap_iphone.py
@@ -13,18 +13,35 @@ class Colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+def scan_network(ip_range):
+    print(Colors.GREEN + "scanning local network using nmap..." + Colors.ENDC)
+    result = subprocess.run(['nmap', '-sn', ip_range], capture_output=True, text=True)
+    lines = result.stdout.splitlines()
+    ips = []
+
+    for i in range(len(lines)):
+        if 'nmap scan report for' in lines[i].lower():
+            ip = lines[i].split()[-1].replace("(", "").replace(")", "")
+            print (Colors.YELLOW + ip + Colors.ENDC)
+            ips.append(ip)
+
+    return ips
+
 # nmap scan
 def scan_iphone(target_ip):
     print(Colors.BLUE + f"[+] scanning {target_ip} for iPhone..." + Colors.ENDC)
     # scan nmap for search iPhone
     result = subprocess.check_output(
-        ["nmap", "-sV", "-Pn", "-T4", "-A", "--script", "http-headers", "-p7000", target_ip],
+        ["nmap", "-sV", "-Pn", "-T4", "-A", "-p62078", target_ip],
         stderr=subprocess.DEVNULL
     ).decode('utf-8')
 
+
+    print (result.lower())
     # search "iPhone"
-    m = re.search(r"62078/tcp (filtered|open)", result.lower())
-    if m:
+    # if "iphone" in result.lower():
+    match = re.search(r"62078/tcp (filtered|open)", result.lower())
+    if match:
         print(Colors.GREEN + f"[+] found iphone with ios at {target_ip}" + Colors.ENDC)
         print (result.lower())
     else:
@@ -32,9 +49,10 @@ def scan_iphone(target_ip):
 
 # discover devices in network
 def discover_devices(ip_range="192.168.8.0/24"):
+    ips = scan_network(ip_range)
+    print (ips)
     print(Colors.BLUE + f"[+] scanning network {ip_range}..." + Colors.ENDC)
-    for i in range(1, 255):
-        target_ip = f"192.168.8.{i}"
+    for target_ip in ips:
         scan_iphone(target_ip)
 
 if __name__ == "__main__":
