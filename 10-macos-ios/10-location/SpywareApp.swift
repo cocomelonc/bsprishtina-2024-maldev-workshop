6d2829731205d972340c4cc3d191539e8449bf5d
cocomelonc <cocomelonkz@gmail.com>
2025-05-13 07:49:29 +0300

    ios location example

---
+++ 10-macos-ios/10-location/SpywareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpywareApp.swift
+//  Spyware
+//
+//  Created by mozzz on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpywareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
